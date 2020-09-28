87d4aaf4bb60d59f40e56206c775eccad13d75b5
Nico Rey <rey.nico@gmail.com>
2020-09-28 20:51:59 -0300

    add is_active to grey out old passes on passes list

--- panel.sql
+++ panel.sql
@@ -2,7 +2,8 @@ create table predict_passes(
     sat_name text not null,
     pass_start timestamp primary key default (strftime('%s', 'now')) not null,
     pass_end timestamp default (strftime('%s', 'now')) not null,
-    max_elev int not null);
+    max_elev int not null,
+    is_active boolean);
 
 
 CREATE TABLE decoded_passes(
