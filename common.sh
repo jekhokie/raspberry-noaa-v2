53c15355f4136c5947aa10a9f831bd29a44a4124
Nico Rey <rey.nico@gmail.com>
2020-09-28 20:51:13 -0300

    fix folder path

--- common.sh
+++ common.sh
@@ -25,7 +25,6 @@ log() {
 
 ## current date and folder structure
 START_DATE=$(date '+%d-%m-%Y %H:%M')
-FOLDER_DATE="$(date +%Y)/$(date +%m)/$(date +%d)"
 
 ## sane checks
 if [ ! -d "${NOAA_HOME}" ]; then
@@ -49,8 +48,8 @@ if [ ! -d "${RAMFS_AUDIO}/audio/" ]; then
 	mkdir -m 775 -p "${RAMFS_AUDIO}/audio/"
 fi
 
-if [ ! -d "${NOAA_OUTPUT}/image/" ]; then
-	mkdir -m 775 -p "${NOAA_OUTPUT}/image/"
+if [ ! -d "${NOAA_OUTPUT}/images/" ]; then
+	mkdir -m 775 -p "${NOAA_OUTPUT}/images/"
 fi
 
 if [ ! -d "${NOAA_HOME}/map/" ]; then
@@ -59,8 +58,4 @@ fi
 
 if [ ! -d "${NOAA_HOME}/predict/" ]; then
 	mkdir -m 775 -p "${NOAA_HOME}/predict/"
-fi
-
-if [ ! -d "${NOAA_OUTPUT}/image/${FOLDER_DATE}" ]; then
-        mkdir -m 775 -p "${NOAA_OUTPUT}/image/${FOLDER_DATE}"
 fi
