ab361f90f274c1663affe05fa031a2621d52cc6f
Nico Rey <rey.nico@gmail.com>
2020-10-07 21:03:49 -0300

    Fix audio path

--- receive.sh
+++ receive.sh
@@ -26,7 +26,7 @@ fi
 # $7 = Satellite max elevation
 
 log "Starting rtl_fm record" "INFO"
-timeout "${6}" /usr/local/bin/rtl_fm ${BIAS_TEE} -f "${2}"M -s 60k -g 50 -p 55 -E wav -E deemp -F 9 - | /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 60000 - "${NOAA_AUDIO}/audio/${3}.wav" rate 11025
+timeout "${6}" /usr/local/bin/rtl_fm ${BIAS_TEE} -f "${2}"M -s 60k -g 50 -p 55 -E wav -E deemp -F 9 - | /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 60000 - "${RAMFS_AUDIO}/audio/${3}.wav" rate 11025
 
 if [ ! -d "{NOAA_OUTPUT}/image/${FOLDER_DATE}" ]; then
 	mkdir -m 775 -p "${NOAA_OUTPUT}/image/${FOLDER_DATE}"
@@ -42,7 +42,7 @@ log "Bulding pass map" "INFO"
 /usr/local/bin/wxmap -T "${1}" -H "${4}" -p 0 -l 0 -o "${PASS_START}" "${NOAA_HOME}/map/${3}-map.png"
 for i in $ENHANCEMENTS; do
 	log "Decoding image" "INFO"
-	/usr/local/bin/wxtoimg -o -m "${NOAA_HOME}/map/${3}-map.png" -e "$i" "${NOAA_AUDIO}/audio/${3}.wav" "${NOAA_OUTPUT}/image/${FOLDER_DATE}/${3}-$i.jpg"
+	/usr/local/bin/wxtoimg -o -m "${NOAA_HOME}/map/${3}-map.png" -e "$i" "${RAMFS_AUDIO}/audio/${3}.wav" "${NOAA_OUTPUT}/image/${FOLDER_DATE}/${3}-$i.jpg"
 	/usr/bin/convert -quality 90 -format jpg "${NOAA_OUTPUT}/image/${FOLDER_DATE}/${3}-$i.jpg" -undercolor black -fill yellow -pointsize 18 -annotate +20+20 "${1} $i ${START_DATE}" "${NOAA_OUTPUT}/image/${FOLDER_DATE}/${3}-$i.jpg"
 done
 if [ -n "$CONSUMER_KEY" ]; then
@@ -56,8 +56,8 @@ fi
 
 if [ "$DELETE_AUDIO" = true ]; then
 	log "Deleting audio files" "INFO"
-    rm "${NOAA_AUDIO}/audio/${3}.wav"
+    rm "${RAMFS_AUDIO}/audio/${3}.wav"
 else
 	log "Moving audio files out of the SD card" "INFO"
-    mv "${NOAA_AUDIO}/audio/${3}.wav" "${NOAA_OUTPUT}/audio/${3}.wav"
+    mv "${RAMFS_AUDIO}/audio/${3}.wav" "${NOAA_OUTPUT}/audio/${3}.wav"
 fi
