6fcffb1925bcaa3a13ff2f09b58d377aaed0004f
Justin Karimi <jekhokie@gmail.com>
2021-02-01 19:40:10 -0500

    Refactor and Consolidate Scheduling Scripts
    Update all scheduling scripts to DRY up the code significantly and
    reduce the total footprint for easier maintenance.

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -1,39 +1,52 @@
 #!/bin/bash
+#
+# High-level scheduling script - schedules all desired satellite and orbital captures.
 
-### Run as a normal user
+# run as a non-root user
 if [ $EUID -eq 0 ]; then
-    echo "This script shouldn't be run as root."
-    exit 1
+  echo "This script shouldn't be run as root."
+  exit 1
 fi
 
-## import common lib
+# import common lib
 . "$HOME/.noaa-v2.conf"
 . "$NOAA_HOME/scripts/common.sh"
 
-wget -qr http://www.celestrak.com/NORAD/elements/weather.txt -O "${NOAA_HOME}"/tmp/weather.txt
-wget -qr http://www.celestrak.com/NORAD/elements/amateur.txt -O "${NOAA_HOME}"/tmp/amateur.txt
+# some constants
+WEATHER_TXT="${NOAA_HOME}/tmp/weather.txt"
+AMATEUR_TXT="${NOAA_HOME}/tmp/amateur.txt"
+TLE_OUTPUT="${NOAA_HOME}/tmp/orbit.tle"
 
-# it's really unfortunate but a directory structure any deeper than 'tmp' in the below
-# results in a buffer overflow reported by the predict application, presumably because
-# it cannot handle that level of sub-directory
-grep "NOAA 15" "${NOAA_HOME}"/tmp/weather.txt -A 2 > "${NOAA_HOME}"/tmp/weather.tle
-grep "NOAA 18" "${NOAA_HOME}"/tmp/weather.txt -A 2 >> "${NOAA_HOME}"/tmp/weather.tle
-grep "NOAA 19" "${NOAA_HOME}"/tmp/weather.txt -A 2 >> "${NOAA_HOME}"/tmp/weather.tle
-grep "METEOR-M 2" "${NOAA_HOME}"/tmp/weather.txt -A 2 >> "${NOAA_HOME}"/tmp/weather.tle
+# get the txt files for orbit information
+wget -qr http://www.celestrak.com/NORAD/elements/weather.txt -O "${WEATHER_TXT}"
+wget -qr http://www.celestrak.com/NORAD/elements/amateur.txt -O "${AMATEUR_TXT}"
+
+# create tle files for scheduling
+#   note: it's really unfortunate but a directory structure any deeper than 'tmp' in the
+#   below results in a buffer overflow reported by the predict application, presumably
+#   because it cannot handle that level of sub-directory
+log "Clearing and re-creating TLE file with latest..." "INFO"
+echo -n "" > $TLE_OUTPUT
 if [ "$SCHEDULE_ISS" == "true" ]; then
-    grep "ZARYA" "${NOAA_HOME}"/tmp/amateur.txt -A 2 > "${NOAA_HOME}"/tmp/amateur.tle
+    grep "ZARYA" $AMATEUR_TXT -A 2 >> $TLE_OUTPUT
 fi
+grep "NOAA 15" $WEATHER_TXT -A 2 >> $TLE_OUTPUT
+grep "NOAA 18" $WEATHER_TXT -A 2 >> $TLE_OUTPUT
+grep "NOAA 19" $WEATHER_TXT -A 2 >> $TLE_OUTPUT
+grep "METEOR-M 2" $WEATHER_TXT -A 2 >> $TLE_OUTPUT
 
-#Remove all AT jobs
+# remove 'at' jobs to make way for new jobs
+log "Clearing existing scheduled 'at' capture jobs..." "INFO"
 for i in $(atq | awk '{print $1}'); do
   atrm "$i"
 done
 
-#Schedule Satellite Passes:
+# create schedules to call respective receive scripts
+log "Scheduling new capture jobs..." "INFO"
 if [ "$SCHEDULE_ISS" == "true" ]; then
-    "${NOAA_HOME}"/scripts/schedule_iss.sh "ISS (ZARYA)" 145.8000
+  $NOAA_HOME/scripts/schedule_captures.sh "ISS (ZARYA)" 145.8000 "receive_iss.sh" $TLE_OUTPUT
 fi
-"${NOAA_HOME}"/scripts/schedule_meteor.sh "METEOR-M 2" 137.1000
-"${NOAA_HOME}"/scripts/schedule_sat.sh "NOAA 19" 137.1000
-"${NOAA_HOME}"/scripts/schedule_sat.sh "NOAA 18" 137.9125
-"${NOAA_HOME}"/scripts/schedule_sat.sh "NOAA 15" 137.6200
+$NOAA_HOME/scripts/schedule_captures.sh "NOAA 15" 137.6200 "receive_noaa.sh" $TLE_OUTPUT
+$NOAA_HOME/scripts/schedule_captures.sh "NOAA 18" 137.9125 "receive_noaa.sh" $TLE_OUTPUT
+$NOAA_HOME/scripts/schedule_captures.sh "NOAA 19" 137.1000 "receive_noaa.sh" $TLE_OUTPUT
+$NOAA_HOME/scripts/schedule_captures.sh "METEOR-M 2" 137.1000 "receive_meteor.sh" $TLE_OUTPUT
