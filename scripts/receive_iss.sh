e28556854506bff54c73bc26aec62451ad2ea0fa
Justin Karimi <jekhokie@gmail.com>
2021-02-05 16:15:32 -0500

    Update Receive Scripts for Spectrogram Output
    Update all receive scripts to account for producing spectrograms, and
    perform a major overhaul. This may have broken them, but they will be
    tested with incoming audio files in the next round of passes and broken
    pieces resolved incrementally.

--- scripts/receive_iss.sh
+++ scripts/receive_iss.sh
@@ -2,8 +2,8 @@
 
 ### Run as a normal user
 if [ $EUID -eq 0 ]; then
-    echo "This script shouldn't be run as root."
-    exit 1
+  echo "This script shouldn't be run as root."
+  exit 1
 fi
 
 ## import common lib
@@ -11,46 +11,54 @@ fi
 . "$HOME/.tweepy.conf"
 . "$NOAA_HOME/scripts/common.sh"
 
-if pgrep "rtl_fm" > /dev/null
-then
-    log "There is an already running rtl_fm instance but I dont care for now, I prefer this pass" "INFO"
-    pkill -9 -f rtl_fm
+# input params
+ISS_NAME=$1
+FREQ=$2
+FILENAME=$3
+EPOCH_START=$5
+CAPTURE_TIME=$6
+ISS_MAX_ELEVATION=$7
+
+if pgrep "rtl_fm" > /dev/null; then
+  log "There is an already running rtl_fm instance but I dont care for now, I prefer this pass" "INFO"
+  pkill -9 -f rtl_fm
 fi
 
-# $1 = Satellite Name
-# $2 = Frequency
-# $3 = FileName base
-# $4 = TLE File
-# $5 = EPOC start time
-# $6 = Time to capture
-# $7 = Satellite max elevation
-
 log "Starting rtl_fm record" "INFO"
-timeout "${6}" /usr/local/bin/rtl_fm ${BIAS_TEE} -M fm -f 145.8M -s 48k -g $GAIN -E dc -E wav -E deemp -F 9 - | sox -t raw -r 48k -c 1 -b 16 -e s - -t wav "${NOAA_AUDIO_OUTPUT}/${3}.wav" rate 11025
+timeout "${CAPTURE_TIME}" /usr/local/bin/rtl_fm ${BIAS_TEE} -M fm -f "${FREQ}"M -s 48k -g $GAIN -E dc -E wav -E deemp -F 9 - | sox -t raw -r 48k -c 1 -b 16 -e s - -t wav "${NOAA_AUDIO_OUTPUT}/${FILENAME}.wav" rate 11025
+
+if [[ "$PRODUCE_SPECTROGRAM}" == "true" ]]; then
+  log "Producing spectrogram" "INFO"
+  spectrogram_text="${START_DATE} @ ${ISS_MAX_ELEVATION}°"
+  sox "${NOAA_AUDIO_OUTPUT}/${FILENAME}.wav" -n spectrogram -t "${SAT_NAME}" -x 1024 -y 257 -c "${spectrogram_text}" -o "${IMAGE_OUTPUT}/${FILENAME}-spectrogram.png"
+  /usr/bin/convert -thumbnail 300 "${IMAGE_OUTPUT}/${FILENAME}-spectrogram.png" "${IMAGE_OUTPUT}/thumb/${FILENAME}-spectrogram.png"
+  sqlite3 $DB_FILE "INSERT INTO decoded_passes (pass_start, file_path, daylight_pass, sat_type, has_spectrogram) VALUES ($EPOCH_START, \"$FILENAME\", 1, 0, 1);"
+fi
 
 if [ -f "$NOAA_HOME/scripts/demod.py" ]; then
-    log "Decoding ISS pass" "INFO"
-    python3 "$NOAA_HOME/scripts/demod.py" "${NOAA_AUDIO_OUTPUT}/${3}.wav" "${IMAGE_OUTPUT}/"
-    decoded_pictures="$(find ${IMAGE_OUTPUT}/ -iname "${3}*png")"
-    img_count=0
-    for image in $decoded_pictures; do
-        log "Decoded image: $image" "INFO"
-        ((img_count++))
-    done
+  log "Decoding ISS pass" "INFO"
+  python3 "$NOAA_HOME/scripts/demod.py" "${NOAA_AUDIO_OUTPUT}/${FILENAME}.wav" "${IMAGE_OUTPUT}/"
+  decoded_pictures="$(find ${IMAGE_OUTPUT}/ -iname "${FILENAME}*png")"
+  img_count=0
+  for image in $decoded_pictures; do
+    log "Decoded image: $image" "INFO"
+    ((img_count++))
+  done
 
-    if [ "$img_count" -gt 0 ]; then
-        /usr/bin/convert -thumbnail 300 "${IMAGE_OUTPUT}/${3}-0.png" "${IMAGE_OUTPUT}/thumb/${3}-0.png"
-        sqlite3 "$DB_FILE" "insert into decoded_passes (pass_start, file_path, daylight_pass, sat_type, img_count) values ($5,\"$3\",1,2,$img_count);"
-        pass_id=$(sqlite3 "$DB_FILE" "select id from decoded_passes order by id desc limit 1;")
-        if [ -n "$CONSUMER_KEY" ]; then
-            log "Posting to Twitter" "INFO"
-            if [ "$img_count" -eq 1 ]; then
-                python3 $NOAA_HOME/scripts/post.py "$1 ${START_DATE} Resolución completa: https://weather.reyni.co/detail.php?id=$pass_id" "$7" "${IMAGE_OUTPUT}/${3}-0.png"
-            elif [ "$img_count" -eq 2 ]; then
-                /usr/bin/convert -thumbnail 300 "${IMAGE_OUTPUT}/${3}-1.png" "${IMAGE_OUTPUT}/thumb/${3}-1.png"
-                python3 $NOAA_HOME/scripts/post.py "$1 ${START_DATE} Mas imagenes: https://weather.reyni.co/detail.php?id=$pass_id" "$7" "${IMAGE_OUTPUT}/${3}-0.png" "${IMAGE_OUTPUT}/${3}-1.png"
-            fi
-        fi
-        sqlite3 "$DB_FILE" "update predict_passes set is_active = 0 where (predict_passes.pass_start) in (select predict_passes.pass_start from predict_passes inner join decoded_passes on predict_passes.pass_start = decoded_passes.pass_start where decoded_passes.id = $pass_id);"
+  if [ "$img_count" -gt 0 ]; then
+    /usr/bin/convert -thumbnail 300 "${IMAGE_OUTPUT}/${FILENAME}-0.png" "${IMAGE_OUTPUT}/thumb/${FILENAME}-0.png"
+    sqlite3 "$DB_FILE" "INSERT OR REPLACE INTO decoded_passes (pass_start, file_path, daylight_pass, sat_type, img_count) VALUES ($EPOCH_START, \"$FILENAME\", 1, 2, $img_count);"
+    pass_id=$(sqlite3 "$DB_FILE" "select id from decoded_passes order by id desc limit 1;")
+    sqlite3 "$DB_FILE" "UPDATE predict_passes SET is_active = 0 WHERE (predict_passes.pass_start) in (select predict_passes.pass_start from predict_passes inner join decoded_passes on predict_passes.pass_start = decoded_passes.pass_start where decoded_passes.id = $pass_id);"
+
+    if [ -n "$CONSUMER_KEY" ]; then
+      log "Posting to Twitter" "INFO"
+      if [ "$img_count" -eq 1 ]; then
+        python3 $NOAA_HOME/scripts/post.py "$ISS_NAME ${START_DATE} Resolución completa: https://weather.reyni.co/detail.php?id=$pass_id" "$ISS_MAX_ELEVATION" "${IMAGE_OUTPUT}/${FILENAME}-0.png"
+      elif [ "$img_count" -eq 2 ]; then
+        /usr/bin/convert -thumbnail 300 "${IMAGE_OUTPUT}/${FILENAME}-1.png" "${IMAGE_OUTPUT}/thumb/${FILENAME}-1.png"
+        python3 $NOAA_HOME/scripts/post.py "$ISS_NAME ${START_DATE} Mas imagenes: https://weather.reyni.co/detail.php?id=$pass_id" "$ISS_MAX_ELEVATION" "${IMAGE_OUTPUT}/${FILENAME}-0.png" "${IMAGE_OUTPUT}/${FILENAME}-1.png"
+      fi
     fi
+  fi
 fi
--- scripts/receive_noaa.sh
+++ scripts/receive_noaa.sh
@@ -2,8 +2,8 @@
 
 ### Run as a normal user
 if [ $EUID -eq 0 ]; then
-    echo "This script shouldn't be run as root."
-    exit 1
+  echo "This script shouldn't be run as root."
+  exit 1
 fi
 
 ## import common lib
@@ -11,27 +11,37 @@ fi
 . "$HOME/.tweepy.conf"
 . "$NOAA_HOME/scripts/common.sh"
 
+# input params
+SAT_NAME=$1
+FREQ=$2
+FILENAME_BASE=$3
+TLE_FILE=$4
+EPOCH_START=$5
+CAPTURE_TIME=$6
+SAT_MAX_ELEVATION=$7
+
+# base directory plus filename_base for re-use
+FILENAME="${NOAA_AUDIO_OUTPUT}/${FILENAME_BASE}"
 
 ## pass start timestamp and sun elevation
-PASS_START=$(expr "$5" + 90)
+PASS_START=$(expr "$EPOCH_START" + 90)
 SUN_ELEV=$(python3 "$NOAA_HOME"/scripts/sun.py "$PASS_START")
 
-if pgrep "rtl_fm" > /dev/null
-then
+if pgrep "rtl_fm" > /dev/null; then
   log "There is an existing rtl_fm instance running, I quit" "ERROR"
   exit 1
 fi
 
-# $1 = Satellite Name
-# $2 = Frequency
-# $3 = FileName base
-# $4 = TLE File
-# $5 = EPOC start time
-# $6 = Time to capture
-# $7 = Satellite max elevation
-
 log "Starting rtl_fm record" "INFO"
-timeout "${6}" /usr/local/bin/rtl_fm ${BIAS_TEE} -f "${2}"M -s 60k -g $GAIN -E wav -E deemp -F 9 - | /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 60000 - "${RAMFS_AUDIO}/${3}.wav" rate 11025
+timeout "${CAPTURE_TIME}" /usr/local/bin/rtl_fm ${BIAS_TEE} -f "${FREQ}"M -s 60k -g $GAIN -E wav -E deemp -F 9 - | /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 60000 - "${FILENAME}.wav" rate 11025
+
+if [[ "$PRODUCE_SPECTROGRAM}" == "true" ]]; then
+  log "Producing spectrogram" "INFO"
+  spectrogram_text="${START_DATE} @ ${SAT_MAX_ELEVATION}°"
+  sox "${FILENAME}.wav" -n spectrogram -t "${SAT_NAME}" -x 1024 -y 257 -c "${spectrogram_text}" -o "${IMAGE_OUTPUT}/${FILENAME_BASE}-spectrogram.png"
+  /usr/bin/convert -thumbnail 300 "${IMAGE_OUTPUT}/${FILENAME_BASE}-spectrogram.png" "${IMAGE_OUTPUT}/thumb/${FILENAME_BASE}-spectrogram.png"
+  sqlite3 $DB_FILE "INSERT INTO decoded_passes (pass_start, file_path, daylight_pass, sat_type, has_spectrogram) VALUES ($EPOCH_START, \"$FILENAME_BASE\", 1, 0, 1);"
+fi
 
 if [ "${SUN_ELEV}" -gt "${SUN_MIN_ELEV}" ]; then
   ENHANCEMENTS="ZA MCIR MCIR-precip MSA MSA-precip HVC-precip HVCT-precip HVC HVCT"
@@ -58,22 +68,23 @@ if [ "${NOAA_MAP_GRID_DEGREES}" != "0.0" ]; then
 fi
 
 # build overlay map
-/usr/local/bin/wxmap -T "${1}" -H "${4}" -p 0 ${extra_map_opts} -o "${epoch_adjusted}" "${NOAA_HOME}/tmp/map/${3}-map.png"
+/usr/local/bin/wxmap -T "${SAT_NAME}" -H "${TLE_FILE}" -p 0 ${extra_map_opts} -o "${epoch_adjusted}" "${NOAA_HOME}/tmp/map/${FILENAME}-map.png"
 
 # build images based on enhancements defined
 for i in $ENHANCEMENTS; do
   log "Decoding image" "INFO"
-  /usr/local/bin/wxtoimg -o -m "${NOAA_HOME}/tmp/map/${3}-map.png" -e "$i" "${RAMFS_AUDIO}/${3}.wav" "${IMAGE_OUTPUT}/${3}-$i.jpg"
-  /usr/bin/convert -quality 90 -format jpg "${IMAGE_OUTPUT}/${3}-$i.jpg" -undercolor black -fill yellow -pointsize 18 -annotate +20+20 "${1} $i ${START_DATE} Elev: $7°" "${IMAGE_OUTPUT}/${3}-$i.jpg"
-  /usr/bin/convert -thumbnail 300 "${IMAGE_OUTPUT}/${3}-$i.jpg" "${IMAGE_OUTPUT}/thumb/${3}-$i.jpg"
+  /usr/local/bin/wxtoimg -o -m "${NOAA_HOME}/tmp/map/${FILENAME_BASE}-map.png" -e "$i" "${FILENAME_BASE}.wav" "${IMAGE_OUTPUT}/${FILENAME_BASE}-$i.jpg"
+  /usr/bin/convert -quality 90 -format jpg "${IMAGE_OUTPUT}/${FILENAME_BASE}-$i.jpg" -undercolor black -fill yellow -pointsize 18 -annotate +20+20 "${SAT_NAME} $i ${START_DATE} Elev: $SAT_MAX_ELEVATION°" "${IMAGE_OUTPUT}/${FILENAME_BASE}-$i.jpg"
+  /usr/bin/convert -thumbnail 300 "${IMAGE_OUTPUT}/${FILENAME_BASE}-$i.jpg" "${IMAGE_OUTPUT}/thumb/${FILENAME_BASE}-$i.jpg"
 done
 
-rm "${NOAA_HOME}/tmp/map/${3}-map.png"
+rm "${NOAA_HOME}/tmp/map/${FILENAME_BASE}-map.png"
 
+# store enhancements
 if [ "${SUN_ELEV}" -gt "${SUN_MIN_ELEV}" ]; then
-  sqlite3 $DB_FILE "insert into decoded_passes (pass_start, file_path, daylight_pass, sat_type) values ($5,\"$3\", 1,1);"
+  sqlite3 $DB_FILE "INSERT OR REPLACE INTO decoded_passes (pass_start, file_path, daylight_pass, sat_type) VALUES ($EPOCH_START, \"$FILENAME_BASE\", 1, 1);"
 else
-  sqlite3 $DB_FILE "insert into decoded_passes (pass_start, file_path, daylight_pass, sat_type) values ($5,\"$3\", 0,1);"
+  sqlite3 $DB_FILE "INSERT OR REPLACE INTO decoded_passes (pass_start, file_path, daylight_pass, sat_type) VALUES ($EPOCH_START, \"$FILENAME_BASE\", 0, 1);"
 fi
 
 pass_id=$(sqlite3 $DB_FILE "select id from decoded_passes order by id desc limit 1;")
@@ -81,9 +92,9 @@ pass_id=$(sqlite3 $DB_FILE "select id from decoded_passes order by id desc limit
 if [ -n "$CONSUMER_KEY" ]; then
   log "Posting to Twitter" "INFO"
   if [ "${SUN_ELEV}" -gt "${SUN_MIN_ELEV}" ]; then
-    python3 "${NOAA_HOME}/scripts/post.py" "$1 ${START_DATE} Mas imagenes: https://weather.reyni.co/detail.php?id=$pass_id" "$7" "${IMAGE_OUTPUT}/$3-MCIR-precip.jpg" "${IMAGE_OUTPUT}/$3-MSA-precip.jpg" "${IMAGE_OUTPUT}/$3-HVC-precip.jpg" "${IMAGE_OUTPUT}/$3-HVCT-precip.jpg"
+    python3 "${NOAA_HOME}/scripts/post.py" "$SAT_NAME ${START_DATE} Mas imagenes: https://weather.reyni.co/detail.php?id=$pass_id" "$SAT_MAX_ELEVATION" "${IMAGE_OUTPUT}/$FILENAME_BASE-MCIR-precip.jpg" "${IMAGE_OUTPUT}/$FILENAME_BASE-MSA-precip.jpg" "${IMAGE_OUTPUT}/$FILENAME_BASE-HVC-precip.jpg" "${IMAGE_OUTPUT}/$FILENAME_BASE-HVCT-precip.jpg"
   else
-    python3 "$NOAA_HOME/scripts/post.py" "$1 ${START_DATE} Mas imagenes: https://weather.reyni.co/detail.php?id=$pass_id" "$7" "${IMAGE_OUTPUT}/$3-MCIR-precip.jpg" "${IMAGE_OUTPUT}/$3-MCIR.jpg"
+    python3 "$NOAA_HOME/scripts/post.py" "$SAT_NAME ${START_DATE} Mas imagenes: https://weather.reyni.co/detail.php?id=$pass_id" "$SAT_MAX_ELEVATION" "${IMAGE_OUTPUT}/$FILENAME_BASE-MCIR-precip.jpg" "${IMAGE_OUTPUT}/$FILENAME_BASE-MCIR.jpg"
   fi
 fi
 
@@ -91,8 +102,5 @@ sqlite3 $DB_FILE "update predict_passes set is_active = 0 where (predict_passes.
 
 if [ "$DELETE_AUDIO" = true ]; then
   log "Deleting audio files" "INFO"
-  rm "${RAMFS_AUDIO}/${3}.wav"
-else
-  log "Moving audio files out to the SD card" "INFO"
-  mv "${RAMFS_AUDIO}/${3}.wav" "${NOAA_AUDIO_OUTPUT}/${3}.wav"
+  rm "${FILENAME}.wav"
 fi
