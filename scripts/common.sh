5e9012c5f803caf4a247dae389450236856ea39f
Justin Karimi <jekhokie@gmail.com>
2021-02-06 20:17:14 -0500

    Fix Up common.sh Script
    Update comments and add clarity around a few things in the common.sh
    script.

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Purpose: Common code that is likely loaded in most of the scripts
+#          within this framework. Handles things such as a start date/time,
+#          logging, and other various "common" functionality.
 
 . "$HOME/.noaa-v2.conf"
 
@@ -10,15 +14,15 @@ log() {
   local log_message=$1
   local log_priority=$2
 
-  #check if level exists
+  # check if level exists and is of the right level to log
   [[ ${levels[$log_priority]} ]] || return 1
-
-  #check if level is enough
   (( ${levels[$log_priority]} < ${levels[$log_level]} )) && return 2
 
-  #log here
+  # log in place (which for at jobs end up in the linux mail)
   echo "${log_priority} : ${log_message}"
+
+  # TODO: log to log file to help with processing outputs (Issue #27)
 }
 
-## current date and folder structure
+# current date and time
 START_DATE=$(date '+%d-%m-%Y %H:%M')
