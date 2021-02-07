1c1c6cb8f799e71b5dab150193d6b64f76f8e269
Justin Karimi <jekhokie@gmail.com>
2021-02-06 20:26:17 -0500

    Clean Up Prune Script
    Few semantic and syntax-based updates for the prune functionality.

--- scripts/prune.sh
+++ scripts/prune.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+#
+# Purpose: Prunes (removes) the 10 oldest captures from the database
+#          and respective images.
 
-## import common lib
+# import common lib
 . "$HOME/.noaa-v2.conf"
 . "$NOAA_HOME/scripts/common.sh"
 
+log "Pruning captures..." "INFO"
 for img_path in $(sqlite3 ${DB_FILE} "select file_path from decoded_passes limit 10;"); do
-    find "${IMAGE_OUTPUT}/" -type f -name "${IMG_NAME}*.jpg" -exec rm -f {} \;
-    find "${IMAGE_OUTPUT}/thumb/" -type f -name "${IMG_NAME}*.jpg" -exec rm -f {} \;
-    log "${img_path} file pruned" "INFO"
-    sqlite3 "${DB_FILE}" "delete from decoded_passes where file_path = \"$img_path\";"
-    log "Database entry pruned" "INFO"
+  find "${IMAGE_OUTPUT}/" -type f -name "${IMG_NAME}*.jpg" -exec rm -f {} \;
+  find "${IMAGE_OUTPUT}/thumb/" -type f -name "${IMG_NAME}*.jpg" -exec rm -f {} \;
+  log "  ${img_path} file pruned" "INFO"
+  sqlite3 "${DB_FILE}" "delete from decoded_passes where file_path = \"$img_path\";"
+  log "  Database entry pruned" "INFO"
 done
