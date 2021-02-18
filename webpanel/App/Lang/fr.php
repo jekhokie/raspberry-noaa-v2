95f35bd11bbc884dd797c0ea6c8cb1bddbb8c20b
Justin Karimi <jekhokie@gmail.com>
2021-02-18 12:57:36 -0500

    Add French Language Translation for Webpanel
    Add the French language translation to the webpanel.

---
+++ webpanel/App/Lang/fr.php
@@ -0,0 +1,27 @@
+<?php
+return array(
+  "admin" => "Admin",
+  "admin_delete_confirm_header" => "Confirmez Supression",
+  "admin_delete_warning" => "Cliquez syr le 'X' à côté des captures que vous souhaitez supprimer pour que la capture soit supprimée de la base de données et que toutes les images associcées soient supprimées ** ATTENTION ** : Cette action est irréversible, assurez-vous que c'est ce que vous voulez faire !",
+  "cancel" => "Annulez",
+  "captures" => "Capture",
+  "confirm" => "Confirmez",
+  "conflicting_pass" => "Conflit avec le passage pécédent",
+  "elev" => "Elévation",
+  "fail_delete_missing_id" => "Echec de la suppresion - paramètre "id" manquant",
+  "image" => "Image",
+  "images" => "Images",
+  "max_elev" => "Elévation Max",
+  "next" => "Suivant",
+  "of" => "of",
+  "page" => "Page",
+  "pass_end" => "Pass Fin",
+  "pass_start" => "Pass Début",
+  "pass_start_azimuth" => "Azimuth",
+  "passes" => "Passes",
+  "prev" => "Préced.",
+  "satellite" => "Satellite",
+  "successful_delete_capture" => "Capture supprimée avec succès",
+  "travel_direction" => "Direction",
+);
+?>
