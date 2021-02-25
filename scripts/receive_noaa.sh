165194e32746b27a28e53bc91db32ad788b92005
Colin Kaminski <32990352+colinluthier@users.noreply.github.com>
2021-02-25 12:03:17 -0800

    Update receive_noaa.sh

--- scripts/receive_noaa.sh
+++ scripts/receive_noaa.sh
@@ -168,7 +168,9 @@ for enhancement in $ENHANCEMENTS; do
     ${IMAGE_PROC_DIR}/noaa_normalize_annotate.sh "${IMAGE_FILE_BASE}-$enhancement.jpg" "${annotation}" 90 >> $NOAA_LOG 2>&1
     ${IMAGE_PROC_DIR}/thumbnail.sh 300 "${IMAGE_FILE_BASE}-$enhancement.jpg" "${IMAGE_THUMB_BASE}-$enhancement.jpg" >> $NOAA_LOG 2>&1
 
-    if [ -f "${IMAGE_FILE_BASE}-$enhancement.jpg" ]; then
+    filesize=$(wc -c "${IMAGE_FILE_BASE}-$enhancement.jpg" | awk '{print $1}')
+    if [ $filesize > 20480 ]; then
+     if [ -f "${IMAGE_FILE_BASE}-$enhancement.jpg" ]; then
       if [ "${ENABLE_EMAIL_PUSH}" == "true" ]; then
         log "Emailing image enhancement $enhancement" "INFO"
         ${PUSH_PROC_DIR}/push_email.sh "${EMAIL_PUSH_ADDRESS}" "${IMAGE_FILE_BASE}-$enhancement.jpg" "${annotation}" >> $NOAA_LOG 2>&1
@@ -178,20 +180,21 @@ for enhancement in $ENHANCEMENTS; do
         log "Pushing image enhancement $enhancement to Discord" "INFO"
         ${PUSH_PROC_DIR}/push_discord.sh "${IMAGE_FILE_BASE}-$enhancement.jpg" "${annotation}" >> $NOAA_LOG 2>&1
       fi
+     fi 
     else
-      log "No image with enhancement $enhancement created - not pushing anywhere" "INFO"
-    fi
+      log "No image with enhancement $enhancement created - not pushing anywhere" "INFO
   fi
-done
+ done
 
 rm "${NOAA_HOME}/tmp/map/${FILENAME_BASE}-map.png"
 
 # store enhancements
-$SQLITE3 $DB_FILE "INSERT OR REPLACE INTO decoded_passes (pass_start, file_path, daylight_pass, sat_type, has_spectrogram, has_pristine) \
+if [ $filesize > 20480 ]; then
+   $SQLITE3 $DB_FILE "INSERT OR REPLACE INTO decoded_passes (pass_start, file_path, daylight_pass, sat_type, has_spectrogram, has_pristine) \
                                      VALUES ($EPOCH_START, \"$FILENAME_BASE\", $daylight, 1, $spectrogram, $pristine);"
 
-pass_id=$($SQLITE3 $DB_FILE "SELECT id FROM decoded_passes ORDER BY id DESC LIMIT 1;")
-$SQLITE3 $DB_FILE "UPDATE predict_passes \
+   pass_id=$($SQLITE3 $DB_FILE "SELECT id FROM decoded_passes ORDER BY id DESC LIMIT 1;")
+   $SQLITE3 $DB_FILE "UPDATE predict_passes \
                    SET is_active = 0 \
                    WHERE (predict_passes.pass_start) \
                    IN ( \
@@ -201,6 +204,7 @@ $SQLITE3 $DB_FILE "UPDATE predict_passes \
                      ON predict_passes.pass_start = decoded_passes.pass_start \
                      WHERE decoded_passes.id = $pass_id \
                    );"
+fi
 
 if [ "$DELETE_AUDIO" = true ]; then
   log "Deleting audio files" "INFO"
