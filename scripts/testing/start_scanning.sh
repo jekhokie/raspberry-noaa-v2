c2ce995c9ae0153d6ac0264b1821185bbfc3d698
Pascal <Cirromulus@users.noreply.github.com>
2021-03-02 17:06:59 +0100

    Use sdr-id and some cleanup. Closes #212

--- scripts/testing/start_scanning.sh
+++ scripts/testing/start_scanning.sh
@@ -16,7 +16,10 @@
 
 [ $# -lt 1 ] && outfile=scan_$(date  +"%d.%m.%y-%H.%M").csv.gz || outfile=$1	#important: outfile needs to end with csv.gz
 
-range="137M:138M:1k" #"130M:140M:5k"
+scriptpath="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# Change frequency here if you want to scan more/less.
+range="137M:138M:1k" # Format: StartFreq:StopFreq:Resolution
 
 echo "
 $(tput setaf 2)
@@ -30,4 +33,4 @@ $(tput setaf 3)
 	(note that gzip output is buffered in chunks, so it won't be updated immediately)
 $(tput sgr0)"
 
-nohup rtl_power ${BIAS_TEE} -f $range -g $GAIN -c 25% -d 0 2> /dev/null | gzip > $outfile &
+nohup rtl_power ${BIAS_TEE} -f $range -g $GAIN -c 25% -d ${SDR_DEVICE_ID} 2> /dev/null | gzip > $outfile &
