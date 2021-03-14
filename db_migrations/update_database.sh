15cc323b9a64d1f02109d93b66e0c293d732a2cc
Colin Kaminski <32990352+colinluthier@users.noreply.github.com>
2021-03-14 16:03:38 -0700

    Update update_database.sh

--- db_migrations/update_database.sh
+++ db_migrations/update_database.sh
@@ -79,4 +79,13 @@ else
   log "  - ${script} already applied" "INFO"
 fi
 
+script="07_add_histogram_bool.sql"
+check=$($SQL_CMD $NOAA_HOME/db/panel.db ".schema decoded_passes" | grep 'has_histogram')
+if [ -z "${check}" ]; then
+  log "  - applying ${script}" "INFO"
+  $SQL_CMD $NOAA_HOME/db/panel.db < $NOAA_HOME/db_migrations/$script
+else
+  log "  - ${script} already applied" "INFO"
+fi
+
 log "Schema updates complete!" "INFO"
