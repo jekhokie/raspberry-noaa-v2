15d9d5090d212d0758c1c5034714b01b6bcecadc
Justin Karimi <jekhokie@gmail.com>
2021-03-14 21:13:17 -0400

    Update Histogram Script for Pathing
    Clean up the histogram scirpt a bit and handle pathing (previously only
    handled local files without a fully-qualified path).

--- scripts/image_processors/histogram.sh
+++ scripts/image_processors/histogram.sh
@@ -9,7 +9,7 @@
 #   4. Additional comment (lower left) for the histogram chart
 #
 # Example:
-#   ./histogram.sh /path/to/inputfile.png /path/to/outputfile.jpg "my chart title" "my chart comment"
+#   ./histogram.sh /path/to/inputfile.jpg /path/to/outputfile.jpg "my chart title" "my chart comment"
 
 # import common lib and settings
 . "$HOME/.noaa-v2.conf"
@@ -21,15 +21,20 @@ OUT_FILE=$2
 CHART_TITLE=$3
 CHART_COMMENT=$4
 
-# produce the histogram on a pristine image. 
-# ToDO: This should be updated to strip the telemetry first. And produce two histograms, one ofr each channel.
-gmic "${IN_FILE}" +histogram 256 display_graph[-1] 400,300,1,0,255,0 outputp
-created_file="_${IN_FILE%.*}~.jpg"
-convert "${created_file}" "${OUT_FILE}"
-rm "${created_file}"
-rm "_${IN_FILE}"
-convert "${OUT_FILE}" -undercolor grey85 -fill black -pointsize 12 -annotate +5+12  "${CHART_TITLE}" "${OUT_FILE}"
-convert "${OUT_FILE}" -gravity SouthWest -undercolor grey85 -fill black -pointsize 12 -annotate +5+1 "${CHART_COMMENT}" "${OUT_FILE}"
+# do some file/extension extraction and temp file variable creation
+# to be used for cleaning up gmic output
+input_file=$(basename "${IN_FILE}")
+input_path=$(dirname "${IN_FILE}")
+input_filename="${input_file%.*}"
+input_fileext="${input_file##*.}"
+gmic_temp_1="${input_path}/_${input_filename}~.${input_fileext}"
+gmic_temp_2="${input_path}/_${input_file}"
 
-
-
+# produce the histogram on a pristine image
+# TODO: This should be updated to strip the telemetry first and produce two histograms, one for each channel
+$GMIC "${IN_FILE}" +histogram 256 display_graph[-1] 400,300,1,0,255,0 outputp
+$CONVERT "${gmic_temp_1}" "${OUT_FILE}"
+rm "${gmic_temp_1}"
+rm "${gmic_temp_2}"
+$CONVERT "${OUT_FILE}" -undercolor grey85 -fill black -pointsize 12 -annotate +5+12  "${CHART_TITLE}" "${OUT_FILE}"
+$CONVERT "${OUT_FILE}" -gravity SouthWest -undercolor grey85 -fill black -pointsize 12 -annotate +5+1 "${CHART_COMMENT}" "${OUT_FILE}"
