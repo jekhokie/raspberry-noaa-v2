54ad1397e3c085a8e3f163b951de27448977f01b
showdoi <showdoi@126.com>
2021-08-30 15:31:52 +0800

    Add support for Chinese
    Add support for Chinese 
    cn.php

---
+++ webpanel/App/Lang/cn.php
@@ -0,0 +1,34 @@
+<?php
+return array(
+  "admin" => "后台管理",
+  "admin_delete_confirm_header" => "确认删除",
+  "admin_capture_delete_warning" => "单击要删除的文件旁边的“X”以从数据库中删除捕获图像并删除所有关联的图像。
+  \n **警告**：这是不可逆的，请谨慎!",
+  "admin_pass_delete_warning" => "单击要删除的文件旁边的“X”以取消计划并将其从数据库中删除。
+  \n -只有出现红色警告说明与某卫星过境时间重叠可做相应选择删除-
+  \n **警告**：这是不可逆的，请谨慎!",
+  "cancel" => "取消",
+  "captures" => "捕获图像",
+  "confirm" => "确定",
+  "conflicting_pass" => "与以前的过境卫星冲突",
+  "elev" => "海拔高度",
+  "enhancement" => "加强",
+  "fail_delete_missing_id" => "无法删除，丢失ID参数",
+  "gain" => "增益",
+  "image" => "解析图像",
+  "images" => "解析相册",
+  "max_elev" => "最大仰角",
+  "next" => "下一个",
+  "of" => "共",
+  "page" => "第",
+  "pass_end" => "过境结束",
+  "pass_start" => "过境开始",
+  "pass_start_azimuth" => "过境方位角",
+  "passes" => "解码任务",
+  "prev" => "上一个",
+  "satellite" => "卫星列表",
+  "successful_delete_capture" => "成功删除图片",
+  "successful_delete_pass" => "成功去除解析过境卫星解码任务",
+  "travel_direction" => "方向",
+);
+?>
