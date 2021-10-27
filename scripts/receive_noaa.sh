4c9be71f77ec702713f9f9dfe9b50d59578f386d
dom-robinson <dom@id3as.co.uk>
2021-10-27 10:09:48 +0100

    yesterday update forgot change pristine source to jpg for thumbnail

--- scripts/receive_noaa.sh
+++ scripts/receive_noaa.sh
@@ -101,7 +101,7 @@ if [[ "${PRODUCE_NOAA_PRISTINE}" == "true" ]]; then
   log "Producing pristine image" "INFO"
   pristine=1
   ${IMAGE_PROC_DIR}/noaa_pristine.sh "${AUDIO_FILE_BASE}.wav" "${IMAGE_FILE_BASE}-pristine.jpg" >> $NOAA_LOG 2>&1
-  ${IMAGE_PROC_DIR}/thumbnail.sh 300 "${IMAGE_FILE_BASE}-pristine.png" "${IMAGE_THUMB_BASE}-pristine.jpg" >> $NOAA_LOG 2>&1
+  ${IMAGE_PROC_DIR}/thumbnail.sh 300 "${IMAGE_FILE_BASE}-pristine.jpg" "${IMAGE_THUMB_BASE}-pristine.jpg" >> $NOAA_LOG 2>&1
 fi
 
 histogram=0
