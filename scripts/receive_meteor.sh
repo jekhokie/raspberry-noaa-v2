39cf6b8b84f4bc63de8c71bc04ad2d0ec4bf4d31
Mihajlo Petković <mihajlo2003petkovic@users.noreply.github.com>
2022-01-16 23:22:55 +0100

    Fixed GNU Radio section. It finally works with MeteorDemod!!!
    I've just fixed GNU Radio section. Now MeteorDemod works on both GNU Radio and RTL_FM!

--- scripts/receive_meteor.sh
+++ scripts/receive_meteor.sh
@@ -233,10 +233,11 @@ else
 fi
 if [ "$METEOR_RECEIVER" == "gnuradio" ]; then
 
-  qpsk_file="${NOAA_HOME}/tmp/meteor/${FILENAME_BASE}.qpsk"
+#  qpsk_file="${NOAA_HOME}/tmp/meteor/${FILENAME_BASE}.s"
 
   log "Starting gnuradio record" "INFO"
-  ${AUDIO_PROC_DIR}/meteor_record_gnuradio.sh $CAPTURE_TIME "$qpsk_file" >> $NOAA_LOG 2>&1
+#  ${AUDIO_PROC_DIR}/meteor_record_gnuradio.sh $CAPTURE_TIME "$qpsk_file" >> $NOAA_LOG 2>&1
+  ${AUDIO_PROC_DIR}/meteor_record_gnuradio.sh $CAPTURE_TIME "${RAMFS_AUDIO_BASE}.s" >> $NOAA_LOG 2>&1
 
   log "Waiting for files to close" "INFO"
   sleep 2
@@ -278,7 +279,8 @@ if [ "$METEOR_RECEIVER" == "gnuradio" ]; then
 
   log "Running MeteorDemod to demodulate QPSK file, rectify (spread) images, create heat map and composites and convert them to JPG" "INFO"
 
-  $METEORDEMOD -t "$TLE_FILE" -f jpg -i "$qpsk_file" >> $NOAA_LOG 2>&1
+#  $METEORDEMOD -t "$TLE_FILE" -f jpg -i "$qpsk_file" >> $NOAA_LOG 2>&1
+  $METEORDEMOD -t "$TLE_FILE" -f jpg -i "${RAMFS_AUDIO_BASE}.s" >> $NOAA_LOG 2>&1
 
   rm *.gcp *.bmp
 
