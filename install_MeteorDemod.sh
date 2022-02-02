9735f24c0a7c3cd9667b824be8bea4d750e5ea7b
Colin Kaminski <32990352+colinluthier@users.noreply.github.com>
2022-02-02 08:49:08 -0800

    Remove build directories before building
    Fixes bug where updating the build fails.

--- install_MeteorDemod.sh
+++ install_MeteorDemod.sh
@@ -5,6 +5,10 @@ sudo apt-get update
 sudo apt install python3-dev python3-pip
 sudo apt install cmake build-essential pkg-config git
 sudo apt install libatlas-base-dev liblapacke-dev gfortran
+
+sudo rm -r opencv
+sudo rm -r MeteorDemod
+
 git clone https://github.com/opencv/opencv.git
 cd opencv/
 mkdir build && cd build
