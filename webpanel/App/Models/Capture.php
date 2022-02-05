2df431e4bfbc3a96a2bdabc684d5c455525ba26d
Mihajlo Petković <mihajlo2003petkovic@users.noreply.github.com>
2022-02-05 12:49:48 +0100

    Updated Capture.php so that it accepts rain overlay created by MeteorDemod

--- webpanel/App/Models/Capture.php
+++ webpanel/App/Models/Capture.php
@@ -70,9 +70,9 @@ class Capture extends \Lib\Model {
     switch($pass['sat_type']) {
       case 0: // Meteor-M2
         if ($pass['daylight_pass'] == 1) {
-          $enhancements = ['-2-122-rectified.jpg','-1-122-rectified.jpg','-3-122-rectified.jpg','-4-122-rectified.jpg','-6-122-rectified.jpg','-5-122-rectified.jpg','-7-122-rectified.jpg','-9-122-rectified.jpg','-8-122-rectified.jpg','-10-122-rectified.jpg','-12-122-rectified.jpg','-11-122-rectified.jpg',];
+          $enhancements = ['-1-122-rectified.jpg','-2-122-rectified.jpg','-3-122-rectified.jpg','-4-122-rectified.jpg','-5-122-rectified.jpg','-6-122-rectified.jpg','-7-122-rectified.jpg','-8-122-rectified.jpg','-9-122-rectified.jpg','-10-122-rectified.jpg','-11-122-rectified.jpg','-12-122-rectified.jpg','-13-122-rectified.jpg','-14-122-rectified.jpg','-15-122-rectified.jpg','-16-122-rectified.jpg','-17-122-rectified.jpg','-18-122-rectified.jpg','-19-122-rectified.jpg','-20-122-rectified.jpg','-21-122-rectified.jpg'];
         } else {
-          $enhancements = ['-2-122-rectified.jpg','-2-122-rectified.jpg','-4-122-rectified.jpg','-3-122-rectified.jpg','-6-122-rectified.jpg','-5-122-rectified.jpg'];
+          $enhancements = ['-1-122-rectified.jpg','-2-122-rectified.jpg','-3-122-rectified.jpg','-4-122-rectified.jpg','-5-122-rectified.jpg','-6-122-rectified.jpg','-7-122-rectified.jpg','-8-122-rectified.jpg','-9-122-rectified.jpg'];
         }
         break;
       case 1: // NOAA
