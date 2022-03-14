ec9226b6117265fc3e1feb4088bc02cabcdbeadc
Mihajlo Petković <mihajlo2003petkovic@users.noreply.github.com>
2022-03-14 21:09:40 +0100

    Commenting out automatic SSL certificate acquisition

--- install_and_upgrade.sh
+++ install_and_upgrade.sh
@@ -113,14 +113,14 @@ else
   die "  Something failed with the install - please inspect the logs above"
 fi
 
-log_running "Installing SSL certificates..."
-if [ $? -eq 0 ] && [ $ENABLE_TLS == "true" ] && [ -n $WEB_SERVER_NAME ]; then
-  sudo certbot certonly --webroot -w /var/www/wx-new/public -d $WEB_SERVER_NAME
-  log_running "Restarting NGINX web server..."
-  sudo systemctl restart nginx
-else
-  die "  Something failed with the install - please inspect the logs above"
-fi
+#log_running "Installing SSL certificates..."
+#if [ $? -eq 0 ] && [ $ENABLE_TLS == "true" ] && [ -n $WEB_SERVER_NAME ]; then
+#  sudo certbot certonly --webroot -w /var/www/wx-new/public -d $WEB_SERVER_NAME
+#  log_running "Restarting NGINX web server..."
+#  sudo systemctl restart nginx
+#else
+#  die "  Something failed with the install - please inspect the logs above"
+#fi
 
 # TLE data files
 # NOTE: This should be DRY-ed up with the scripts/schedule.sh script
