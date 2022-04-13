78ad36d9add6ee98037e3c5c81938b00536ed0c8
Colin Kaminski <32990352+colinluthier@users.noreply.github.com>
2022-04-13 14:37:28 -0700

    Update install_MeteorDemod.sh

--- install_MeteorDemod.sh
+++ install_MeteorDemod.sh
@@ -9,7 +9,7 @@ git clone https://github.com/opencv/opencv.git
 cd opencv/
 mkdir build && cd build
 cmake ../  -DBUILD_LIST=core,imgproc,imgcodecs -DCMAKE_INSTALL_PREFIX=/usr/local -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_SHARED_LINKER_FLAGS=-latomic
-make -j4
+make -j2
 sudo make install
 
 cd ~
@@ -18,7 +18,7 @@ cd MeteorDemod
 git submodule update --init --recursive
 mkdir build && cd build
 cmake ../
-make -j4
+make -j2
 sudo make install
 sudo chown $USER:$USER -R ~/.config/meteordemod
 
