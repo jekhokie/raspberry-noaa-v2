856deb3c94c081a443c34ee9f7ced115adae8786
dom.robinson <dom@id3as.co.uk>
2022-09-20 10:31:33 +0100

    single quote prune scripts

--- scripts/prune_scripts/prune_older_than.sh
+++ scripts/prune_scripts/prune_older_than.sh
@@ -12,8 +12,8 @@ let prunedate=$(date +%s)-$PRUNE_OLDER_THAN*24*60*60
 
 log "Pruning captures..." "INFO"
 for img_path in $(sqlite3 ${DB_FILE} "select file_path from decoded_passes where pass_start < $prunedate;"); do
-  find "${IMAGE_OUTPUT}/" -maxdepth 1 -type f -name "${img_path}*.jpg" exec rm -f {};
-  find "${IMAGE_OUTPUT}/thumb/" -maxdepth 1 -type f -name "${img_path}*.jpg" exec rm -f {};
+  find "${IMAGE_OUTPUT}/" -maxdepth 1 -type f -name '${img_path}*.jpg' exec rm -f {};
+  find "${IMAGE_OUTPUT}/thumb/" -maxdepth 1 -type f -name '${img_path}*.jpg' exec rm -f {};
   log "  ${img_path} file pruned" "INFO"
   sqlite3 "${DB_FILE}" "delete from decoded_passes where file_path = \"$img_path\";"
   log "  Database entry pruned" "INFO"
--- scripts/prune_scripts/prune_oldest.sh
+++ scripts/prune_scripts/prune_oldest.sh
@@ -9,8 +9,8 @@
 
 log "Pruning captures..." "INFO"
 for img_path in $(sqlite3 ${DB_FILE} "select file_path from decoded_passes limit ${PRUNE_OLDEST};"); do
-  find "${IMAGE_OUTPUT}/" -maxdepth 1 -type f -name "${img_path}*.jpg" exec rm -f {} \;
-  find "${IMAGE_OUTPUT}/thumb/" -maxdepth 1 -type f -name "${img_path}*.jpg" exec rm -f {} \;
+  find "${IMAGE_OUTPUT}/" -maxdepth 1 -type f -name '${img_path}*.jpg' exec rm -f {} \;
+  find "${IMAGE_OUTPUT}/thumb/" -maxdepth 1 -type f -name '${img_path}*.jpg' exec rm -f {} \;
   log "  ${img_path} file pruned" "INFO"
   sqlite3 "${DB_FILE}" "delete from decoded_passes where file_path = \"$img_path\";"
   log "  Database entry pruned" "INFO"
