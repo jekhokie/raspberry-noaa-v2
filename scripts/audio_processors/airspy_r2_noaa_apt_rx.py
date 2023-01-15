f2d718b6a823a58ec160ff58b59ea95b884207bc
Mihajlo Petković <mihajlo2003petkovic@users.noreply.github.com>
2023-01-15 01:26:01 +0100

    Added bias-T support
    Bias T can now be set from the `settings.yml` file.

--- scripts/audio_processors/airspy_r2_noaa_apt_rx.py
+++ scripts/audio_processors/airspy_r2_noaa_apt_rx.py
@@ -79,7 +79,7 @@ class rtlsdr_noaa_apt_rx(gr.top_block):
                 taps=None,
                 fractional_bw=None)
         self.osmosdr_source_0 = osmosdr.source(
-            args="numchan=" + str(1) + " " + "airspy=0,pack=1"
+            args="numchan=" + str(1) + " " + "airspy=0,pack=1" + ",bias=" + bias_t + ""
         )
         self.osmosdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
         self.osmosdr_source_0.set_sample_rate(2500000)
