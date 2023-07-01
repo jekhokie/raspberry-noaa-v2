ad74ca700e65274a99b8bd115342cf0f5b839e46
Mihajlo Petković <mihajlo2003petkovic@users.noreply.github.com>
2023-07-01 18:25:47 +0200

    Airspy Mini reception of Meteor M2-3

---
+++ scripts/audio_processors/airspy_mini_m2_lrpt_rx.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Airspy Mini Meteor QPSK LRPT
+# Author: Mihajlo
+# GNU Radio version: 3.8.2.0
+
+from datetime import datetime
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import filter
+from gnuradio.filter import firdes
+from gnuradio import gr
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import osmosdr
+import time
+
+
+class airspy_mini_m2_lrpt_rx(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Airspy Mini Meteor QPSK LRPT")
+        
+        stream_name = sys.argv[1]
+        gain = float(sys.argv[2])
+        freq_offset = int(sys.argv[3])
+        sdr_dev_id = sys.argv[4]
+        bias_t_string = sys.argv[5]
+        bias_t = "1"
+        if not bias_t_string:
+          bias_t = "0"
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate_airspy = samp_rate_airspy = 3e6
+        self.interp = interp = 5
+        self.decim = decim = 96
+        self.symb_rate = symb_rate = 72000
+        self.samp_rate = samp_rate = samp_rate_airspy*interp/decim
+        self.sps = sps = (samp_rate*1.0)/(symb_rate*1.0)
+        self.pll_alpha = pll_alpha = 0.006
+        self.freq = freq = 137900000
+        self.clock_alpha = clock_alpha = 0.002
+        self.bitstream_name = bitstream_name = stream_name
+
+        ###############################################################
+        # Blocks - note the fcd_freq, freq_offset rtl device, bias-t and gain are carried
+        #          in from settings.yml using the 'variables' block above.
+        #          NOTE: If you edit and replace this .py in gnucomposer
+        #          these will be overwritten with hard-coded values and
+        #          need to be manually reintroduced to make the script take
+        #          settings from your own settings.yml.
+        ################################################################
+        self.root_raised_cosine_filter_0 = filter.fir_filter_ccf(
+            1,
+            firdes.root_raised_cosine(
+                1,
+                samp_rate,
+                symb_rate,
+                0.6,
+                361))
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=interp,
+                decimation=decim,
+                taps=None,
+                fractional_bw=None)
+        self.osmosdr_source_0 = osmosdr.source(
+            args="numchan=" + str(1) + " " + "airspy=0,pack=1"
+        )
+        self.osmosdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
+        self.osmosdr_source_0.set_sample_rate(samp_rate_airspy)
+        self.osmosdr_source_0.set_center_freq(freq, 0)
+        self.osmosdr_source_0.set_freq_corr(68, 0)
+        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
+        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
+        self.osmosdr_source_0.set_gain_mode(False, 0)
+        self.osmosdr_source_0.set_gain(20, 0)
+        self.osmosdr_source_0.set_if_gain(10, 0)
+        self.osmosdr_source_0.set_bb_gain(20, 0)
+        self.osmosdr_source_0.set_antenna('', 0)
+        self.osmosdr_source_0.set_bandwidth(1500000, 0)
+        self.digital_costas_loop_cc_0 = digital.costas_loop_cc(pll_alpha, 4, False)
+        self.digital_constellation_soft_decoder_cf_1 = digital.constellation_soft_decoder_cf(digital.constellation_calcdist(([-1-1j, -1+1j, 1+1j, 1-1j]), ([0, 1, 3, 2]), 4, 1).base())
+        self.digital_clock_recovery_mm_xx_0 = digital.clock_recovery_mm_cc(sps, clock_alpha**2/4.0, 0.5, clock_alpha, 0.005)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_float_to_char_0 = blocks.float_to_char(1, 127)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, bitstream_name, False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.blocks_delay_0 = blocks.delay(gr.sizeof_float*1, 1)
+        self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
+        self.analog_rail_ff_0 = analog.rail_ff(-1, 1)
+        self.analog_agc_xx_0 = analog.agc_cc(1000e-4, 0.5, 1.0)
+        self.analog_agc_xx_0.set_max_gain(4000)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_agc_xx_0, 0), (self.root_raised_cosine_filter_0, 0))
+        self.connect((self.analog_rail_ff_0, 0), (self.blocks_float_to_char_0, 0))
+        self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_delay_0, 0))
+        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_delay_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.blocks_float_to_char_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.digital_clock_recovery_mm_xx_0, 0))
+        self.connect((self.digital_clock_recovery_mm_xx_0, 0), (self.digital_constellation_soft_decoder_cf_1, 0))
+        self.connect((self.digital_constellation_soft_decoder_cf_1, 0), (self.analog_rail_ff_0, 0))
+        self.connect((self.digital_costas_loop_cc_0, 0), (self.blocks_complex_to_float_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_agc_xx_0, 0))
+        self.connect((self.root_raised_cosine_filter_0, 0), (self.digital_costas_loop_cc_0, 0))
+
+
+    def get_samp_rate_airspy(self):
+        return self.samp_rate_airspy
+
+    def set_samp_rate_airspy(self, samp_rate_airspy):
+        self.samp_rate_airspy = samp_rate_airspy
+        self.set_samp_rate(self.samp_rate_airspy*self.interp/self.decim)
+        self.osmosdr_source_0.set_sample_rate(self.samp_rate_airspy)
+
+    def get_interp(self):
+        return self.interp
+
+    def set_interp(self, interp):
+        self.interp = interp
+        self.set_samp_rate(self.samp_rate_airspy*self.interp/self.decim)
+
+    def get_decim(self):
+        return self.decim
+
+    def set_decim(self, decim):
+        self.decim = decim
+        self.set_samp_rate(self.samp_rate_airspy*self.interp/self.decim)
+
+    def get_symb_rate(self):
+        return self.symb_rate
+
+    def set_symb_rate(self, symb_rate):
+        self.symb_rate = symb_rate
+        self.set_sps((self.samp_rate*1.0)/(self.symb_rate*1.0))
+        self.root_raised_cosine_filter_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate, self.symb_rate, 0.6, 361))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.set_sps((self.samp_rate*1.0)/(self.symb_rate*1.0))
+        self.root_raised_cosine_filter_0.set_taps(firdes.root_raised_cosine(1, self.samp_rate, self.symb_rate, 0.6, 361))
+
+    def get_sps(self):
+        return self.sps
+
+    def set_sps(self, sps):
+        self.sps = sps
+        self.digital_clock_recovery_mm_xx_0.set_omega(self.sps)
+
+    def get_pll_alpha(self):
+        return self.pll_alpha
+
+    def set_pll_alpha(self, pll_alpha):
+        self.pll_alpha = pll_alpha
+        self.digital_costas_loop_cc_0.set_loop_bandwidth(self.pll_alpha)
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.osmosdr_source_0.set_center_freq(self.freq, 0)
+
+    def get_clock_alpha(self):
+        return self.clock_alpha
+
+    def set_clock_alpha(self, clock_alpha):
+        self.clock_alpha = clock_alpha
+        self.digital_clock_recovery_mm_xx_0.set_gain_omega(self.clock_alpha**2/4.0)
+        self.digital_clock_recovery_mm_xx_0.set_gain_mu(self.clock_alpha)
+
+    def get_bitstream_name(self):
+        return self.bitstream_name
+
+    def set_bitstream_name(self, bitstream_name):
+        self.bitstream_name = bitstream_name
+        self.blocks_file_sink_0.open(self.bitstream_name)
+
+
+
+
+
+def main(top_block_cls=airspy_mini_m2_lrpt_rx, options=None):
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
