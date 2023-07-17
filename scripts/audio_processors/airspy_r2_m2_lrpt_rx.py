c96b34f011c2093fd5947e7bf184cc6aca9ae63d
Mihajlo Petković <mihajlo2003petkovic@users.noreply.github.com>
2023-07-17 20:02:32 +0200

    Fix NoGUI issue...

--- scripts/audio_processors/airspy_r2_m2_lrpt_rx.py
+++ scripts/audio_processors/airspy_r2_m2_lrpt_rx.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 #
@@ -6,25 +7,25 @@
 # GNU Radio Python Flow Graph
 # Title: Airspy R2 Meteor QPSK LRPT
 # Author: Mihajlo
-# GNU Radio version: 3.10.6.0
+# GNU Radio version: 3.8.2.0
 
 from gnuradio import blocks
 from gnuradio import filter
 from gnuradio.filter import firdes
 from gnuradio import gr
-from gnuradio.fft import window
 import sys
 import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
 import osmosdr
 import time
 
-class airspy_r2_m2_lrpt_rx(gr.hier_block2):
+
+class airspy_r2_m2_lrpt_rx(gr.top_block):
+
     def __init__(self):
-        gr.hier_block2.__init__(
-            self, "Airspy R2 Meteor QPSK LRPT",
-                gr.io_signature(0, 0, 0),
-                gr.io_signature(0, 0, 0),
-        )
+        gr.top_block.__init__(self, "Airspy R2 Meteor QPSK LRPT")
 
         stream_name = sys.argv[1]
         gain = float(sys.argv[2])
@@ -55,7 +56,7 @@ class airspy_r2_m2_lrpt_rx(gr.hier_block2):
                 interpolation=1,
                 decimation=decim,
                 taps=[],
-                fractional_bw=0)
+                fractional_bw=None)
         self.osmosdr_source_0 = osmosdr.source(args="numchan=" + str(1) + " " + "airspy=0,pack=1,bias=" + bias_t)
         self.osmosdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
         self.osmosdr_source_0.set_sample_rate(samp_rate_airspy)
@@ -69,22 +70,14 @@ class airspy_r2_m2_lrpt_rx(gr.hier_block2):
         self.osmosdr_source_0.set_bb_gain(0, 0)
         self.osmosdr_source_0.set_antenna('', 0)
         self.osmosdr_source_0.set_bandwidth(1500000, 0)
-        self.blocks_wavfile_sink_0 = blocks.wavfile_sink(
-            output_baseband,
-            2,
-            (int(samp_rate_airspy/decim)),
-            blocks.FORMAT_WAV,
-            blocks.FORMAT_PCM_U8,
-            False
-            )
+        self.blocks_wavfile_sink_0 = blocks.wavfile_sink(output_baseband, 2, int(samp_rate_airspy/decim), 8)
         self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
-
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_wavfile_sink_0, 0))
         self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_wavfile_sink_0, 1))
+        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_wavfile_sink_0, 0))
         self.connect((self.osmosdr_source_0, 0), (self.rational_resampler_xxx_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_complex_to_float_0, 0))
 
@@ -109,3 +102,22 @@ class airspy_r2_m2_lrpt_rx(gr.hier_block2):
     def set_decim(self, decim):
         self.decim = decim
 
+def main(top_block_cls=airspy_r2_m2_lrpt_rx, options=None):
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
--- scripts/audio_processors/hackrf_m2_lrpt_rx.py
+++ scripts/audio_processors/hackrf_m2_lrpt_rx.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 #
@@ -6,25 +7,25 @@
 # GNU Radio Python Flow Graph
 # Title: Hackrf Meteor QPSK LRPT
 # Author: Mihajlo
-# GNU Radio version: 3.10.6.0
+# GNU Radio version: 3.8.2.0
 
 from gnuradio import blocks
 from gnuradio import filter
 from gnuradio.filter import firdes
 from gnuradio import gr
-from gnuradio.fft import window
 import sys
 import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
 import osmosdr
 import time
 
-class hackrf_m2_lrpt_rx(gr.hier_block2):
+
+class hackrf_m2_lrpt_rx(gr.top_block):
+
     def __init__(self):
-        gr.hier_block2.__init__(
-            self, "Hackrf Meteor QPSK LRPT",
-                gr.io_signature(0, 0, 0),
-                gr.io_signature(0, 0, 0),
-        )
+        gr.top_block.__init__(self, "Hackrf Meteor QPSK LRPT")
 
         stream_name = sys.argv[1]
         gain = float(sys.argv[2])
@@ -46,12 +47,11 @@ class hackrf_m2_lrpt_rx(gr.hier_block2):
         ##################################################
         # Blocks
         ##################################################
-
         self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
                 interpolation=1,
                 decimation=decim,
                 taps=[],
-                fractional_bw=0)
+                fractional_bw=None)
         self.osmosdr_source_0 = osmosdr.source(args="numchan=" + str(1) + " " + "hackrf=0,bias=" + bias_t)
         self.osmosdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
         self.osmosdr_source_0.set_sample_rate(samp_rate_hackrf)
@@ -65,22 +65,14 @@ class hackrf_m2_lrpt_rx(gr.hier_block2):
         self.osmosdr_source_0.set_bb_gain(20, 0)
         self.osmosdr_source_0.set_antenna('', 0)
         self.osmosdr_source_0.set_bandwidth(1500000, 0)
-        self.blocks_wavfile_sink_0 = blocks.wavfile_sink(
-            output_baseband,
-            2,
-            (int(samp_rate_hackrf/decim)),
-            blocks.FORMAT_WAV,
-            blocks.FORMAT_PCM_U8,
-            False
-            )
+        self.blocks_wavfile_sink_0 = blocks.wavfile_sink(output_baseband, 2, int(samp_rate_hackrf/decim), 8)
         self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
-
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_wavfile_sink_0, 1))
         self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_wavfile_sink_0, 0))
+        self.connect((self.blocks_complex_to_float_0, 1), (self.blocks_wavfile_sink_0, 1))
         self.connect((self.osmosdr_source_0, 0), (self.rational_resampler_xxx_0, 0))
         self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_complex_to_float_0, 0))
 
@@ -105,3 +97,22 @@ class hackrf_m2_lrpt_rx(gr.hier_block2):
     def set_decim(self, decim):
         self.decim = decim
 
+def main(top_block_cls=hackrf_m2_lrpt_rx, options=None):
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
