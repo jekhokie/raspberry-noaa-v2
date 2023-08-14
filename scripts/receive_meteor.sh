1ba7ec26e7e0e7764ca55dd65cf1d5ebd9301047
Mihajlo Petković <mihajlo2003petkovic@users.noreply.github.com>
2023-08-14 13:34:37 +0200

    TESTING NEW FILENAMES FOR METEORDEMOD

--- scripts/receive_meteor.sh
+++ scripts/receive_meteor.sh
@@ -182,11 +182,15 @@ if [ "$METEOR_RECEIVER" == "rtl_fm" ]; then
     $CONVERT -quality 100 $FLIP "$i" "$i" >> $NOAA_LOG 2>&1
   done
 
-  for i in *.jpg; do
-    ${IMAGE_PROC_DIR}/meteor_normalize_annotate.sh "$i" "$i" $METEOR_IMAGE_QUALITY >> $NOAA_LOG 2>&1
-    ${IMAGE_PROC_DIR}/thumbnail.sh 300 "$i" "${i%.jpg}-thumb-122-rectified.jpg" >> $NOAA_LOG 2>&1
-    mv "$i" "${IMAGE_FILE_BASE}-${counter}-122-rectified.jpg"
-    mv "${i%.jpg}-thumb-122-rectified.jpg" "${IMAGE_THUMB_BASE}-${counter}-122-rectified.jpg"
+  counter=1
+  for file in *.jpg; do
+    new_filename=$(echo "$file" | sed -E 's/_[0-9]+-[0-9]+-[0-9]+-[0-9]+-[0-9]+-[0-9]+\.jpg$/.jpg/')
+    mv "$file" "$new_filename"
+
+    ${IMAGE_PROC_DIR}/meteor_normalize_annotate.sh "$new_filename" "$new_filename" $METEOR_IMAGE_QUALITY >> $NOAA_LOG 2>&1
+    ${IMAGE_PROC_DIR}/thumbnail.sh 300 "$new_filename" "${new_filename%.jpg}-thumb-122-rectified.jpg" >> $NOAA_LOG 2>&1
+    mv "$new_filename" "${IMAGE_FILE_BASE}-${counter}-122-rectified.jpg"
+    mv "${new_filename%.jpg}-thumb-122-rectified.jpg" "${IMAGE_THUMB_BASE}-${counter}-122-rectified.jpg"
     push_file_list="$push_file_list ${IMAGE_FILE_BASE}-${counter}-122-rectified.jpg "
     ((counter++))
   done
@@ -222,11 +226,15 @@ elif [ "$METEOR_RECEIVER" == "gnuradio" ]; then
     $CONVERT -quality 100 $FLIP "$i" "$i" >> $NOAA_LOG 2>&1
   done
 
-  for i in *.jpg; do
-    ${IMAGE_PROC_DIR}/meteor_normalize_annotate.sh "$i" "$i" $METEOR_IMAGE_QUALITY >> $NOAA_LOG 2>&1
-    ${IMAGE_PROC_DIR}/thumbnail.sh 300 "$i" "${i%.jpg}-thumb-122-rectified.jpg" >> $NOAA_LOG 2>&1
-    mv "$i" "${IMAGE_FILE_BASE}-${counter}-122-rectified.jpg"
-    mv "${i%.jpg}-thumb-122-rectified.jpg" "${IMAGE_THUMB_BASE}-${counter}-122-rectified.jpg"
+  counter=1
+  for file in *.jpg; do
+    new_filename=$(echo "$file" | sed -E 's/_[0-9]+-[0-9]+-[0-9]+-[0-9]+-[0-9]+-[0-9]+\.jpg$/.jpg/')
+    mv "$file" "$new_filename"
+
+    ${IMAGE_PROC_DIR}/meteor_normalize_annotate.sh "$new_filename" "$new_filename" $METEOR_IMAGE_QUALITY >> $NOAA_LOG 2>&1
+    ${IMAGE_PROC_DIR}/thumbnail.sh 300 "$new_filename" "${new_filename%.jpg}-thumb-122-rectified.jpg" >> $NOAA_LOG 2>&1
+    mv "$new_filename" "${IMAGE_FILE_BASE}-${counter}-122-rectified.jpg"
+    mv "${new_filename%.jpg}-thumb-122-rectified.jpg" "${IMAGE_THUMB_BASE}-${counter}-122-rectified.jpg"
     push_file_list="$push_file_list ${IMAGE_FILE_BASE}-${counter}-122-rectified.jpg "
     ((counter++))
   done
