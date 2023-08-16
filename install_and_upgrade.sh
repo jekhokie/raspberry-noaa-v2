602bab08b8b5d16e2065f18bf2d22869c0998810
Mihajlo Petković <mihajlo2003petkovic@users.noreply.github.com>
2023-08-16 12:59:04 +0200

    This now works.

--- install_and_upgrade.sh
+++ install_and_upgrade.sh
@@ -85,9 +85,6 @@ if [ $? -ne 0 ]; then
   fi
 fi
 
-#log_running "Setting ~/.config/meteordemod ownership to the current user"
-#sudo chown $USER:$USER -R ~/.config/meteordemod
-
 log_running "Checking for configuration settings..."
 if [ -f config/settings.yml ]; then
   log_done "  Settings file ready!"
