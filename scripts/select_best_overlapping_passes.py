29761661b6be29679ae08ab16f78de8fbe48d0c3
Mihajlo Petković <mihajlo2003petkovic@users.noreply.github.com>
2023-08-25 17:48:19 +0200

    Fixed! Now it's working!

--- scripts/select_best_overlapping_passes.py
+++ scripts/select_best_overlapping_passes.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 
-import sqlite3
-from datetime import datetime
+import sqlite3                                  from datetime import datetime
 import os
 import sys
 
 def delete_job(target_datetime_str):
+    print("Overlapping pass at:", target_datetime_str)
+
     # Convert the target datetime string to a datetime object
-    target_datetime = datetime.fromtimestamp(int(target_datetime_str))
+    target_datetime = datetime.strptime(target_datetime_str, "%Y-%m-%d %H:%M:%S")
+    # Set the seconds to 0 for target_datetime
+    target_datetime = target_datetime.replace(second=0)
 
     # Get the list of at jobs
     atq_output = os.popen("atq").read().strip()
     atq_lines = atq_output.split("\n")
-
-    # Loop through the at jobs and find the ones scheduled around the target datetime
-    for line in atq_lines:
+                                                    # Loop through the at jobs and find the ones scheduled around the target datetime               for line in atq_lines:
         job_id, job_datetime = line.split(None, 1)
 
         # Extract the full date and time string
         job_datetime = " ".join(job_datetime.split()[:5])
         job_datetime_obj = datetime.strptime(job_datetime, "%a %b %d %H:%M:%S %Y")
+        # Set the seconds to 0 for job_datetime
+        job_datetime_obj = job_datetime_obj.replace(second=0)
 
         if job_datetime_obj == target_datetime:
             print(f"Removing job with ID: {job_id}")
@@ -41,33 +44,37 @@ if __name__ == "__main__":
         pass_start_curr, pass_end_curr, max_elev_curr, sat_name_curr, at_job_id_curr = passes[i][1], passes[i][2], passes[i][3], passes[i][0], passes[i][8]
         pass_start_next, max_elev_next, sat_name_next, at_job_id_next = passes[i + 1][1], passes[i + 1][3], passes[i + 1][0], passes[i + 1][8]
 
+        # Convert the integer timestamps to string format
+        pass_start_curr_str = datetime.fromtimestamp(pass_start_curr).strftime("%Y-%m-%d %H:%M:%S")
+        pass_start_next_str = datetime.fromtimestamp(pass_start_next).strftime("%Y-%m-%d %H:%M:%S")
+
         if pass_start_curr < pass_start_next and pass_start_next < pass_end_curr:
             if "METEOR" in sat_name_curr or "METEOR" in sat_name_next:
                 if "METEOR" in sat_name_curr and "METEOR" not in sat_name_next:
                     if at_job_id_next != 0:
-                        delete_job(str(pass_start_next))
+                        delete_job(pass_start_next_str)
                         cursor.execute("DELETE FROM predict_passes WHERE pass_start = ?", (pass_start_next,))
                 elif "METEOR" in sat_name_next and "METEOR" not in sat_name_curr:
                     if at_job_id_curr != 0:
-                        delete_job(str(pass_start_curr))
+                        delete_job(pass_start_curr_str)
                         cursor.execute("DELETE FROM predict_passes WHERE pass_start = ?", (pass_start_curr,))
                 elif "METEOR" in sat_name_curr and "METEOR" in sat_name_next:
                     if max_elev_curr > max_elev_next:
                         if at_job_id_next != 0:
-                            delete_job(str(pass_start_next))
+                            delete_job(pass_start_next_str)
                             cursor.execute("DELETE FROM predict_passes WHERE pass_start = ?", (pass_start_next,))
                     else:
                         if at_job_id_curr != 0:
-                            delete_job(str(pass_start_curr))
+                            delete_job(pass_start_curr_str)
                             cursor.execute("DELETE FROM predict_passes WHERE pass_start = ?", (pass_start_curr,))
             else:
                 if max_elev_curr > max_elev_next:
                     if at_job_id_next != 0:
-                        delete_job(str(pass_start_next))
+                        delete_job(pass_start_next_str)
                         cursor.execute("DELETE FROM predict_passes WHERE pass_start = ?", (pass_start_next,))
                 else:
                     if at_job_id_curr != 0:
-                        delete_job(str(pass_start_curr))
+                        delete_job(pass_start_curr_str)
                         cursor.execute("DELETE FROM predict_passes WHERE pass_start = ?", (pass_start_curr,))
 
         conn.commit()
