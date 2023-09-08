39aff1bd93790dadd8f6ca1c70a8fafa4c2f77c2
Mihajlo Petković <mihajlo2003petkovic@users.noreply.github.com>
2023-09-08 12:55:38 +0200

    Adding timezone text to the annotation variable

--- scripts/receive_noaa.sh
+++ scripts/receive_noaa.sh
@@ -21,7 +21,7 @@ TIMER_START=$(date '+%s')
 # import common lib and settings
 . "$HOME/.noaa-v2.conf"
 . "$NOAA_HOME/scripts/common.sh"
-capture_start=$START_DATE
+capture_start="$START_DATE $(date '+%Z')"
 
 # input params
 export SAT_NAME=$1
