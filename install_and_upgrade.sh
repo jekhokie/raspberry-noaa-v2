da19fd19e524c49ac19a4ec067836ebf01c4f23f
Mihajlo Petković <mihajlo2003petkovic@users.noreply.github.com>
2023-09-13 10:37:08 +0200

    Starting integration for PCs and othe usernames

--- install_and_upgrade.sh
+++ install_and_upgrade.sh
@@ -93,7 +93,7 @@ else
 fi
 
 log_running "Running Ansible to install and/or update your raspberry-noaa-v2..."
-ansible-playbook -i ansible/hosts --extra-vars "@config/settings.yml" ansible/site.yml
+ansible-playbook -i ansible/hosts --extra-vars "@config/settings.yml" ansible/site.yml -e "target_user=$USER system_architecture=$(dpkg --print-architecture)"
 if [ $? -eq 0 ]; then
   log_done "  Ansible apply complete!"
 else
