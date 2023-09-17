28bc7b8bb44c99da95b63eafe48322987233fb03
Mihajlo Petković <mihajlo2003petkovic@users.noreply.github.com>
2023-09-17 21:50:42 +0200

    Fixing annotations not getting produced on images.

--- scripts/common.sh
+++ scripts/common.sh
@@ -41,7 +41,7 @@ SOX="/usr/bin/sox"
 SQLITE3="/usr/bin/sqlite3"
 WXMAP="/usr/local/bin/wxmap"
 WXTOIMG="/usr/local/bin/wxtoimg"
-WKHTMLTOIMG="/usr/local/bin/wkhtmltoimage"
+WKHTMLTOIMG="/usr/bin/wkhtmltoimage"
 METEORDEMOD="/usr/local/bin/meteordemod"
 SATDUMP="/usr/bin/satdump"
 
