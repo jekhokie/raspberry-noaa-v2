373bcfbb698ba366a693889bb38d9d4265d2e1ed
MihajloPi <mihajlo.raspberrypi@gmail.com>
2024-04-03 12:00:33 +0200

    Hopefully this fixes the filenames
    The problem was that I have to remove the longest strings, then shortest strings. I was removing it vice versa, and when the shortest strung is a substring in the longest string, it gets removed, and the longer string doesn't match anymore to the orignal hence the `rgb_` ending.

--- scripts/receive_noaa.sh
+++ scripts/receive_noaa.sh
@@ -313,11 +313,10 @@ elif [ "$NOAA_DECODER" == "satdump" ]; then
       continue
     fi
 
-    new_name="${new_file//avhrr_apt_rgb_}"
+    new_name="${new_file//rgb_avhrr_3_rgb_}"
     new_name="${new_name//avhrr_apt_rgb_}"
-    new_name="${new_name//avhrr_apt_}"
     new_name="${new_name//avhrr_3_rgb_}"
-    new_name="${new_name//rgb_avhrr_3_rgb_}"
+    new_name="${new_name//avhrr_apt_}"
     new_name="${new_name//_(channel_1)}"
     new_name="${new_name//_(channel_4)}"
     ${IMAGE_PROC_DIR}/noaa_normalize_annotate.sh "$new_file" "${IMAGE_FILE_BASE}-${new_name%.png}.jpg" $NOAA_IMAGE_QUALITY >> $NOAA_LOG 2>&1
