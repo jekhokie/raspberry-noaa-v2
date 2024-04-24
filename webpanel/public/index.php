ead2ff8de82f73a36c77daac41d2e6276b2847b2
Richard Creasey - AI4Y <69941625+creas002@users.noreply.github.com>
2024-04-24 16:34:49 -0400

    Modified to revert back to full Error reporting for PHP - Update index.php

--- webpanel/public/index.php
+++ webpanel/public/index.php
@@ -6,7 +6,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use Config\Config;
 
 # error handling
-error_reporting(0);
+error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
