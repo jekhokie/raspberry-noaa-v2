05c3d3cf41f54324df8610414f66221376297a96
Richard Creasey - AI4Y <69941625+creas002@users.noreply.github.com>
2024-07-21 06:49:39 -0400

    Introduction of Verification Tool and RN2 uninstall script (#1019)
    * Introduction of verification tool
    
    The verification tool can be used to help identify RN2 installation/configuration issues which may potentially prevent proper functioning f capture/decode/processing of APT telemetry data.
    
    Execute the verification script by passing the required argument [ quick | full ]
    
    $HOME/raspberry-noaa-v2/scripts/tools/verification.sh
    
    Argument required:  ./verification.sh quick    or    ./verification.sh full
                            (~ 1 minute)                       (~ 5 minutes)
    
    #########
    # NOTES #
    #########
    
    # Dryrun of binaries includes executing :
    
      nxing web page returned 200 OK status to confirm Web Portal is up.
    
      satdump live capture for 1 second to ensure it runs without error.
    
      wxmap generates an overlay map image which can be found       : $HOME/raspberry-noaa-v2/scripts/tools/verification_tool/test_files/wxtoimg-map-output.png
    
      wxtoimg generates MCIR enhanced image which can be founnd     :  $HOME/raspberry-noaa-v2/scripts/tools/verification_tool/test_files/wxtoimg-mcir-output.jpg
    
      meteordemod -h is executed to ensure it runs without error.
    
    # When FULL mode is choosen meterdemod fully decodes a staged cadu file :
    
      meteordemod generates a full set of images which can be found :  $HOME/raspberry-noaa-v2/scripts/tools/verification_tool/test_files/tmp
    
    * Set 755 on verification.sh
    
    Set 755 on verification.sh
    
    * Ensure meteordemond working directory exists
    
    * Set 755 on build_permissions_list.sh
    
    * Update README.md
    
    Introduction of new Verification Tool
    
    * Update README.md
    
    Reformatting Troubleshooting section from lastchange
    
    * Update README.md
    
    * Script to remove RN2 installation
    
    * updated to shutdown nginx before removal
    
    * stop php service
    
    * Corrected php service name to stop when removing RN2
    
    * further cleanup of services
    
    * Tweaked some of the logging messages
    
    * updated to remove rtl-sdr package
    
    * install script does not install RTL-SDR when it finds source in /tmp
    
    * Update caution_uninstall_rn2.sh

---
+++ scripts/tools/caution_uninstall_rn2.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+#
+# Purpose: The purpose of this uninstall script is to remove the RN2 environment 
+#          and key packages like satdump, meteordemod, , predict, wxtoimg, and nginx. As well
+#          as removal of RN2 crontab entries and Audio, Videos and Images in /srv
+#
+# Author:  Richard Creasey (AI4Y)
+#
+# Created: July 20th, 2024
+
+start=$(date +%s)
+
+UNINSTALL_LOG=/tmp/uninstall.log
+PACKAGES_BULLSEYE="satdump wxtoimg nginx predict rtl-sdr php7.4-intl php8.0-sqlite3 php8.0-mbstring php8.0-fpm"
+PACKAGES_BOOKWORM="satdump wxtoimg nginx predict rtl-sdr php8.2-intl php8.2-sqlite3 php8.2-mbstring php8.2-fpm"
+PATHS="/srv/audio /srv/videos /srv/images $HOME/.config/composer $HOME/.config/gmic $HOME/.config/matplotlib $HOME/.config/meteordemod $HOME/.config/composer $HOME/.config/satdump $HOME/raspberry-noaa-v2 $HOME/.predict $HOME/.noaa-v2.conf $HOME/.wxtoimglic $HOME/.wxtoimgrc /usr/local/bin/rtl_* /var/log/raspberry-noaa-v2 /etc/sudoers.d/020_www-data-atrm-nopasswd /var/www/wx-new /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default /tmp/rtl-sdr"
+SERVICES="phpsessionclean.service phpsessionclean.timer nginx.service"
+OS=$(grep -E "^deb http://raspbian.raspberry.org/raspbian|^deb http://raspbian.raspberrypi.org/raspbian|^deb http://deb.debian.org/debian|^deb https://deb.debian.org/debian" /etc/apt/sources.list /etc/apt/sources.list.d/official-package-repositories.list 2> /dev/null | head -n 1 | awk '{print $3}')
+
+if [[ ${OS} == "bookworm" ]]; then
+  PACKAGES=${PACKAGES_BOOKWORM}
+elif [[ ${OS} == "bullseye" ]]; then
+  PACKAGES=${PACKAGES_BULLSEYE}
+else
+  echo "Aborting, unsupported Operating System ${OS}"
+  exit 1
+fi 
+
+secs_to_human() {
+    if [[ -z ${1} || ${1} -lt 60 ]] ;then
+        min=0 ; secs="${1}"
+    else
+        time_mins=$(echo "scale=2; ${1}/60" | bc)
+        min=$(echo ${time_mins} | cut -d'.' -f1)
+        secs="0.$(echo ${time_mins} | cut -d'.' -f2)"
+        secs=$(echo ${secs}*60|bc|awk '{print int($1+0.5)}')
+    fi
+    echo "Time Elapsed : ${min} minutes and ${secs} seconds."
+}
+
+# loggit function
+loggit() {
+  local log_type=$1
+  local log_message=$2
+
+  echo "${log_type} : ${log_message}"
+
+  # log output to a log file
+  echo $(date '+%d-%m-%Y %H:%M') "${log_type} : ${log_message}" >> "$UNINSTALL_LOG"
+
+}
+
+# Check Package status
+package_statuses() {
+
+  loggit "INFO" ""
+  loggit "INFO" "------------------------------"
+  loggit "INFO" "Package statues"
+  loggit "INFO" "------------------------------"
+  for package in `echo ${PACKAGES}`;
+  do
+  
+    PkgInstalled=$(sudo dpkg -s ${package} 2>/dev/null | grep -i "Status:" | wc -l)
+  
+    if [[ ${PkgInstalled} -eq 1 ]]; then
+      loggit "INFO" "${package} is installed"
+    else
+      loggit "INFO" "${package} is NOT installed"
+    fi
+ 
+  done
+
+  if [[ -f /usr/local/bin/meteordemod ]]; then
+    loggit "INFO" "meteordemod is installed"
+  else
+    loggit "INFO" "meteordemod is NOT installed"
+  fi
+}
+
+remove_services() {
+
+  loggit "INFO" ""
+  loggit "INFO" "------------------------------"
+  loggit "INFO" "Remove RN2 Services"
+  loggit "INFO" "------------------------------"
+  for svc in `echo ${SERVICES}`;
+  do
+   
+    loggit "INFO" "Stopping and removing ${svc} service"
+    sudo sudo systemctl stop ${svc}
+    sudo sudo systemctl disable ${svc}
+     
+  done
+
+}
+
+# Remove software packages
+remove_packages() {
+
+  loggit "INFO" ""
+  loggit "INFO" "------------------------------"
+  loggit "INFO" "Removing packages"
+  loggit "INFO" "------------------------------"
+  
+  for pkg in `echo ${PACKAGES}`;
+  do
+  
+    v_result=$(sudo apt -y remove --purge ${pkg} 2>&1>/dev/null )
+  
+    PkgInstalled=$(sudo dpkg -s ${pkg} 2>/dev/null | grep -i "Status:" | wc -l)
+ 
+    if [[ ${PkgInstalled} -eq 1 ]]; then
+      loggit "INFO" "${pkg} is installed"
+    else
+      loggit "INFO" "${pkg} is no longer installed"
+    fi
+  
+  done
+
+  sudo rm -rf /usr/local/bin/meteordemod
+  if [[ -f /usr/local/bin/meteordemod ]]; then
+    loggit "INFO" "meteordemod is installed"
+  else
+    loggit "INFO" "meteordemod is no longer installed"
+  fi
+}
+
+remove_paths() {
+
+  loggit "INFO" ""
+  loggit "INFO" "------------------------------"
+  loggit "INFO" "Removing Files and Directories"
+  loggit "INFO" "------------------------------"
+  for path in `echo ${PATHS}`;
+  do
+  
+    sudo rm -rf ${path}
+  
+    if ! (( $? )); then
+      loggit "PASS" "${path} no longer exists"
+    else
+      loggit "FAIL" "${path} failed to remove"
+    fi
+
+  done  
+}
+
+package_statuses
+remove_services
+remove_packages
+remove_paths
+
+loggit "INFO" ""
+loggit "INFO" "------------------------------"
+loggit "INFO" "Removing RN2 CRON jobs"
+loggit "INFO" "------------------------------"
+
+crontab -l  | grep -Ev "schedule.sh|scratch_perms.sh|Ansible" > $HOME/crontab.stripped
+crontab < $HOME/crontab.stripped
+CronQty=$(crontab -l  | grep -Ev "schedule.sh|scratch_perms.sh|Ansible" | wc -l)
+
+if [[ ${CronQty} -eq 0 ]]; then
+  loggit "PASS" "RN2 cronjobs no longer exist"
+  \rm -rf $HOME/crontab.stripped
+else
+  loggit "FAIL" "RN2 cronjobs were NOT removed"
+fi
+
+
+loggit "INFO" ""
+loggit "INFO" "Log of results --> ${UNINSTALL_LOG}"
+echo ""
+
+secs_to_human "$(($(date +%s) - ${start}))"
