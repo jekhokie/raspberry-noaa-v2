ad5555d760191e5e4e37cf7508b5d6f57de5e2cb
Richard Creasey - AI4Y <69941625+creas002@users.noreply.github.com>
2024-07-21 11:43:43 -0400

    RTL-SDR changes since it is not a APT installed package

--- scripts/tools/caution_uninstall_rn2.sh
+++ scripts/tools/caution_uninstall_rn2.sh
@@ -11,8 +11,8 @@
 start=$(date +%s)
 
 UNINSTALL_LOG=/tmp/uninstall.log
-PACKAGES_BULLSEYE="satdump wxtoimg nginx predict rtl-sdr php7.4-intl php8.0-sqlite3 php8.0-mbstring php8.0-fpm"
-PACKAGES_BOOKWORM="satdump wxtoimg nginx predict rtl-sdr php8.2-intl php8.2-sqlite3 php8.2-mbstring php8.2-fpm"
+PACKAGES_BULLSEYE="satdump wxtoimg nginx predict php7.4-intl php8.0-sqlite3 php8.0-mbstring php8.0-fpm"
+PACKAGES_BOOKWORM="satdump wxtoimg nginx predict php8.2-intl php8.2-sqlite3 php8.2-mbstring php8.2-fpm"
 PATHS="/srv/audio /srv/videos /srv/images $HOME/.config/composer $HOME/.config/gmic $HOME/.config/matplotlib $HOME/.config/meteordemod $HOME/.config/composer $HOME/.config/satdump $HOME/raspberry-noaa-v2 $HOME/.predict $HOME/.noaa-v2.conf $HOME/.wxtoimglic $HOME/.wxtoimgrc /usr/local/bin/rtl_* /var/log/raspberry-noaa-v2 /etc/sudoers.d/020_www-data-atrm-nopasswd /var/www/wx-new /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default /tmp/rtl-sdr"
 SERVICES="phpsessionclean.service phpsessionclean.timer nginx.service"
 OS=$(grep -E "^deb http://raspbian.raspberry.org/raspbian|^deb http://raspbian.raspberrypi.org/raspbian|^deb http://deb.debian.org/debian|^deb https://deb.debian.org/debian" /etc/apt/sources.list /etc/apt/sources.list.d/official-package-repositories.list 2> /dev/null | head -n 1 | awk '{print $3}')
